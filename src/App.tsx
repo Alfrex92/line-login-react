5b616334a20cdd49c3ae194fb34f5162ba23d748
Alfred <alfredo.riveroll@linecorp.com>
2022-07-06 15:32:14 -0500

    testing something new

--- src/App.tsx
+++ src/App.tsx
@@ -1,89 +1,41 @@
 import "./App.css";
 import liff from "@line/liff";
 import { useEffect, useState } from "react";
-import { initLineLogin, logoutLineLogin } from "./liff/liff";
+import { initLineLogin } from "./liff/liff";
 
 function App() {
-  const [idToken, setIdToken] = useState<any>("");
-  const [displayName, setDisplayName] = useState("");
-  const [statusMessage, setStatusMessage] = useState<any>("");
-  const [userId, setUserId] = useState("");
-
-  const runApp = () => {
-    const idToken = liff.getIDToken();
-    setIdToken(idToken);
-    liff
-      .getProfile()
-      .then((profile) => {
-        console.log(profile);
-        setDisplayName(profile.displayName);
-        setStatusMessage(profile.statusMessage);
-        setUserId(profile.userId);
-      })
-      .catch((err) => console.error(err));
-  };
+  const [liffInitialized, setLiffInitialized] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [errorInit, setErrorInit] = useState<any>(undefined);
+  console.log("loading", loading);
 
   useEffect(() => {
-    initLineLogin(runApp);
+    async function initializer() {
+      setLoading(true);
+      try {
+        await initLineLogin();
+        setLiffInitialized(true);
+      } catch (e) {
+        setErrorInit(e);
+        console.error("There was an error when trying to init Liff", e);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    initializer();
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div style={{ textAlign: "center" }}>
-          <h1>React with LINE Login test bot1</h1>
-          <hr />
-          <p
-            style={{
-              textAlign: "left",
-              marginLeft: "20%",
-              marginRight: "20%",
-              wordBreak: "break-all",
-            }}
-          >
-            <b>id token: </b> {idToken}
-          </p>
-          <p
-            style={{
-              textAlign: "left",
-              marginLeft: "20%",
-              marginRight: "20%",
-              wordBreak: "break-all",
-            }}
-          >
-            <b>display name: </b> {displayName}
-          </p>
-          <p
-            style={{
-              textAlign: "left",
-              marginLeft: "20%",
-              marginRight: "20%",
-              wordBreak: "break-all",
-            }}
-          >
-            <b>status message: </b> {statusMessage}
-          </p>
-          <p
-            style={{
-              textAlign: "left",
-              marginLeft: "20%",
-              marginRight: "20%",
-              wordBreak: "break-all",
-            }}
-          >
-            <b>user id: </b> {userId}
-          </p>
+  console.log("liffInitialized", liffInitialized);
 
-          <button
-            onClick={() => logoutLineLogin()}
-            style={{ width: "100%", height: 30 }}
-          >
-            Logout
-          </button>
-        </div>
-      </header>
-    </div>
-  );
+  if (errorInit) {
+    return <p>There was an error: {errorInit?.message}</p>;
+  }
+  if (loading) {
+    return <p>Loading</p>;
+  }
+  if (!liff.isLoggedIn()) liff.login();
+  return <p> app bitch</p>;
 }
 
 export default App;
