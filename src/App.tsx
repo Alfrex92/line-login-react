b193f6be9951235bf8cecfcd6f9702a5e6702f15
Alfred <alfredo.riveroll@linecorp.com>
2022-07-06 16:04:49 -0500

    react query added

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,20 @@
 import "./App.css";
 import liff from "@line/liff";
-import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
 import { initLineLogin, logoutLineLogin } from "./liff/liff";
 
 function App() {
-  const [liffInitialized, setLiffInitialized] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [errorInit, setErrorInit] = useState<any>(undefined);
-  console.log("loading", loading);
-
-  useEffect(() => {
-    async function initializer() {
-      setLoading(true);
-      try {
-        await initLineLogin();
-        setLiffInitialized(true);
-      } catch (e) {
-        setErrorInit(e);
-        console.error("There was an error when trying to init Liff", e);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    initializer();
-  }, []);
-
-  console.log("liffInitialized", liffInitialized);
-
-  if (errorInit) {
-    return <p>There was an error: {errorInit?.message}</p>;
-  }
-  if (loading) {
+  const { isLoading, error } = useQuery("initLineLogin", initLineLogin);
+  if (isLoading) {
     return <p>Loading</p>;
   }
+  if (error) {
+    return <p>There was an error:</p>;
+  }
+
   if (!liff.isLoggedIn()) liff.login();
   return (
     <div>
-      {" "}
       <p>app bitch</p>
       <button
         onClick={() => logoutLineLogin()}
